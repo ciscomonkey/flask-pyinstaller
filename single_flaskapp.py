51e6228ba3a2c524d6353ce9b218be37fb6cb6c0
Ryan Mullins <>
2020-01-06 14:25:39 -0600

    Added auto launch browser for reference

--- single_flaskapp.py
+++ single_flaskapp.py
@@ -3,6 +3,8 @@
 
 import sys
 import os
+import webbrowser
+from threading import Timer
 from flask import Flask, render_template
 
 if getattr(sys, 'frozen', False):
@@ -13,10 +15,15 @@ else:
     app = Flask(__name__)
 
 
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+
 @app.route('/')
 @app.route('/<name>')
 def hello_world(name=None):
     return render_template('hello.html', name=name)
 
 
+Timer(1, open_browser).start()
 app.run()
